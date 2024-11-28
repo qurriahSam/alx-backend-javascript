ad1d1dff3feda4c461e570ce1a7fa32b39d4ba43
qurriahSam <mugirethe1@gmail.com>
2024-11-28 12:04:09 +0300

    fix: correct calc checker errors

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,42 +1,36 @@
-const assert = require('assert');
-const mocha = require('mocha');
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
 
-const calculateNumber = require('./0-calcul');
-
-describe('calculateNumber', () => {
-  it('should return sum of integers', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(1, -3), -2);
+describe("calculateNumber", () => {
+  it("Returns 4", () => {
+    assert.equal(calculateNumber(1, 3), 4);
   });
 
-  it('should round floats', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(0.1, 0), 0);
-    assert.strictEqual(calculateNumber(1.4, -4.5), -3);
+  it("Returns 5", () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return the rounded number if only one is provided', () => {
-    assert.strictEqual(calculateNumber(2), 2);
-    assert.strictEqual(calculateNumber(2.7), 3);
+  it("Returns 5", () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should cast non-numbers into numbers', () => {
-    assert.strictEqual(calculateNumber(true, '3'), 4);
-    assert.strictEqual(calculateNumber(1, '3.7'), 5);
-    assert.strictEqual(calculateNumber('1.2', 3.7), 5);
+  it("Returns 6", () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 
-  it('should throw typeerror if either param cannot be coerced to a number', () => {
-    assert.throws(() => calculateNumber('hello'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers'
-    });
-    assert.throws(() => calculateNumber(1.2, 'dog'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers'
-    });
+  it("Returns 3", () => {
+    assert.equal(calculateNumber(0, 3.3), 3);
+  });
+
+  it("Returns 2", () => {
+    assert.equal(calculateNumber(-1.6, 3.7), 2);
+  });
+
+  it("Returns -6", () => {
+    assert.equal(calculateNumber(-1.6, -3.7), -6);
+  });
+
+  it("Returns -1", () => {
+    assert.equal(calculateNumber(1.5, -3.5), -1);
   });
 });
--- 0x06-unittests_in_js/0-main.js
+++ 0x06-unittests_in_js/0-main.js
@@ -1,6 +1,9 @@
-const calculateNumber = require("./0-calcul.js");
-console.log(calculateNumber(1, 3));
-console.log(calculateNumber(1, 3.7));
-console.log(calculateNumber(1.2, 3.7));
-console.log(calculateNumber(1.5, 3.7));
+const calculateNumber = require("./0-calcul")
 
+console.log(calculateNumber("DIVIDE", 1, 4))
+console.log(calculateNumber("SUM", 1, 3.7))
+console.log(calculateNumber("SUM", 1.2, 3.7))
+console.log(calculateNumber("SUM", 1.5, 3.7))
+
+
+console.log(Math.round(-1.6))
